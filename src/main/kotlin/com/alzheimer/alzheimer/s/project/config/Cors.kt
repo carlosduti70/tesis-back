39e2e018683296fa9617369439ce16ab0a61033e
Carlos Zuniga <cmzuniga@sudamericano.edu.ec>
2024-08-03 16:52:41 -0500

    ruta front

--- src/main/kotlin/com/alzheimer/alzheimer/s/project/config/Cors.kt
+++ src/main/kotlin/com/alzheimer/alzheimer/s/project/config/Cors.kt
@@ -11,7 +11,7 @@ class CorsConfig {
     @Bean
     fun corsConfigurationSource(): CorsConfigurationSource? {
         val configuration = CorsConfiguration()
-        configuration.allowedOrigins = listOf("http://localhost:3000")
+        configuration.allowedOrigins = listOf("https://tesis-front-gd7p8t59l-carlosduti70s-projects.vercel.app")
         configuration.allowedMethods = listOf("HEAD",
             "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
         configuration.allowCredentials = true
